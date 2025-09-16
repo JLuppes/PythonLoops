9e61d86f952e6f32bb7348bec1e5c46a71005202
Joshua Luppes <lupjos@dunwoody.edu>
2025-09-15 22:59:11 -0500

    Add new while file

---
+++ while.py
@@ -0,0 +1,45 @@
+# While Loops
+keepPlaying = True
+while keepPlaying:
+    response = input("Keep playing? (y/n):")
+    if response.lower() in ['n', 'no']: keepPlaying = False
+print("Game Over!")
+
+# Run a Certain Amount
+runTimes = 3
+while runTimes > 0:
+    print(f'RunTimes is now {runTimes}')
+    runTimes -= 1
+
+# Entry Controlled
+stuckInASpiral = False
+while stuckInASpiral:
+    print("Help I'm stuck in a spiral!")
+    stuckInASpiral = True
+
+# While True - Break
+while True:
+    response = input("Type 'please' to end loop. (please):")
+    if response.lower() == "please": break
+print("The End!")
+
+# Continue
+countDown = 10
+while countDown > 0:
+    countDown -= 1
+    even = countDown % 2
+    if even == 1: continue
+    print(f"Even numbers only: {countDown}")
+
+# Else
+magicNumber = 3
+countDown = 10
+while countDown > 0:
+    countDown -= 1
+    if countDown == magicNumber:
+        print(f"Found the magic number: {countDown}")
+        break
+    print(f"Countdown: {countDown}")
+else: 
+    print("Magic number not found!")
+    
