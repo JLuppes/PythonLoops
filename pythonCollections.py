be65bd27f48d433882a94d9b7eebac035e006498
Joshua Luppes <lupjos@dunwoody.edu>
2025-09-18 08:54:39 -0500

    Add examples for list collections

--- pythonCollections.py
+++ pythonCollections.py
@@ -3,7 +3,7 @@
 
 ## List
 birdList = ['eagle', 'hawk', 'chickadee']
-print(birdList)
+print(birdList) # ['eagle', 'hawk', 'chickadee']
 
 ### Access a List
 ### Lists are ordered and changeable, duplicates allowed
@@ -14,9 +14,22 @@ print(birdList[0]) # ostrich
 ### Duplicates are allowed
 birdList[1] = "ostrich"
 print(birdList) # ['ostrich', 'ostrich', 'chickadee']
+print(birdList.count("ostrich")) # 2
 
 ### Check length with len()
 print(f"Length of birdList: {len(birdList)}") # 3
+
+### Add New Items
+birdList.append("grackle")
+print(birdList) # ['ostrich', 'ostrich', 'chickadee', 'grackle]
+
+### Remove a Specific Item
+birdList.remove("chickadee")
+print(birdList) # ['ostrich', 'ostrich',  'grackle']
+
+### Remove and return the last item
+print(f"I just caught a {birdList.pop()}!") # I just caught a grackle!
+print(birdList) # ['ostrich', 'ostrich']
 
 ## Tuple
 ### Ordered, unchangeable, duplicates allowed
